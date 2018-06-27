22233033a454db03a7b6e497c25b50fe7a9d92a6
Justin Kenney <justinkenney13@gmail.com>
2018-06-27 17:16:42 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,6 @@
 # require libraries/modules here
 require 'nokogiri'
+require 'pry'
 
 def create_project_hash
   # write your code here
@@ -7,3 +8,5 @@ def create_project_hash
 
   kickstarter = Nokogiri::HTML(html)
 end
+
+binding.pry
