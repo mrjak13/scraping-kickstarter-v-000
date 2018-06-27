32efda2bfa0ebe7e65b02f31574141f0c030cd24
Justin Kenney <justinkenney13@gmail.com>
2018-06-27 18:07:03 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ def create_project_hash
 
   kickstarter = Nokogiri::HTML(html)
 
-  project = kickstarter.css("li.project.grid_4")
+  project = kickstarter.css("li.project.grid_4").first
 
   title = project.css("h2.bbcard_name strong a").text
 
